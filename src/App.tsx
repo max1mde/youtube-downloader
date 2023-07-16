3e2af053399da106461ff1f56a47a8a7b5bcea86
Maxim <maximdeclient@gmail.com>
2023-07-16 20:24:23 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Form from "./components/Form.tsx";
+
+interface DropDown {
+  options: string[];
+}
+
+function App() {
+  const dropDownYT: DropDown[] = [
+    { options: ["MP4", "MP3"] },
+    { options: ["Best available quality", "FullHD if available", "720p", "480", "Shit quality"] },
+  ];
+  return (
+    <div>
+      <img className={"main-logo"} src={"../public/logo.svg"} alt={"logo"}/>
+      <div className={"youtube-downloader"}>
+        <Form name={"YouTube Downloader"} onSubmit={() => null} inputName={"Download any video from YouTube"} inputPlaceholder={"Link https://yout..."} buttonName={"Download"} dropdowns={dropDownYT}></Form>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form.tsx
@@ -0,0 +1,41 @@
+interface DropDown {
+    options: string[];
+}
+
+interface Props {
+    name: string;
+    onSubmit: (link: string, mp4: boolean, quality: string) => void;
+    inputName: string
+    inputPlaceholder: string
+    buttonName: string
+    dropdowns: DropDown[]
+}
+
+function Form({name, onSubmit, inputName, inputPlaceholder, buttonName, dropdowns}: Props) {
+    return (
+        <div>
+            <form>
+                <fieldset>
+                    <legend>{name}</legend>
+                    <div className="mb-3">
+                        <label htmlFor="textInput" className="form-label">{inputName}</label>
+                        <input type="text" id="textInput" className="form-control" placeholder={inputPlaceholder}/>
+                    </div>
+
+                    {dropdowns.map((dropdown) => (
+                        <div className="mb-3">
+                            <select className="form-select">
+                                {dropdown.options.map((option) => (
+                                    <option>{option}</option>
+                                ))}
+                            </select>
+                        </div>
+                    ))}
+                    <button onClick={() => onSubmit("", true, "")} type="submit" className="btn btn-success">{buttonName}</button>
+                </fieldset>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
